f937f42aed7614ddb2e240bddfdafe56b1153886
John Gillen <john.gillen@trojans.dsu.edu>
2022-05-27 18:59:10 -0500

    day 3

---
+++ day03.py
@@ -0,0 +1,45 @@
+from enum import unique
+
+
+f = open("day03.input")
+lines = f.readlines()
+
+houses = [(0, 0)]
+santa = [(0, 0)]
+robosanta = [(0, 0)]
+
+for line in lines:
+    counter = 0
+    for character in line:
+        if character == '<':
+            houses.append((houses[-1][0] - 1, houses[-1][1]))
+            if counter % 2 == 0:
+                santa.append((santa[-1][0] - 1, santa[-1][1]))
+            else:
+                robosanta.append((robosanta[-1][0] - 1, robosanta[-1][1]))
+        if character == '>':
+            houses.append((houses[-1][0] + 1, houses[-1][1]))
+            if counter % 2 == 0:
+                santa.append((santa[-1][0] + 1, santa[-1][1]))
+            else:
+                robosanta.append((robosanta[-1][0] + 1, robosanta[-1][1]))
+        if character == 'v':
+            houses.append((houses[-1][0], houses[-1][1] - 1))
+            if counter % 2 == 0:
+                santa.append((santa[-1][0], santa[-1][1] - 1))
+            else:
+                robosanta.append((robosanta[-1][0], robosanta[-1][1] - 1))
+        if character == '^':
+            houses.append((houses[-1][0], houses[-1][1] + 1))
+            if counter % 2 == 0:
+                santa.append((santa[-1][0], santa[-1][1] + 1))
+            else:
+                robosanta.append((robosanta[-1][0], robosanta[-1][1] + 1))
+        
+        counter += 1
+
+houses = len(list(set(houses)))
+santa = len(list(set(santa + robosanta)))
+
+print("part 1: " + str(houses))
+print("part 2: " + str(santa))
