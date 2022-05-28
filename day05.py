849b61d58d96d797f91d7f2d07720663eabd5534
John Gillen <john.gillen@trojans.dsu.edu>
2022-05-27 19:59:30 -0500

    day 5 part 2

--- day05.py
+++ day05.py
@@ -1,10 +1,10 @@
 import enum
 
-
 f = open("day05.input")
 lines = f.readlines()
 
 nice = 0
+newnice = 0
 
 for line in lines:
     naughty = False
@@ -14,7 +14,6 @@ for line in lines:
     for i, character in enumerate(line):
         if 'aeiou'.find(character) != -1:
             vowels += 1
-        
         if i < len(line) - 1 and line[i] == line[i + 1]:
             consecutive = True
         
@@ -36,4 +35,26 @@ for line in lines:
     if not naughty:
         nice += 1
 
+for line in lines:
+    naughty = False
+
+    doubleconsecutive = False
+    repeatover = False
+    for i, character in enumerate(line):
+        if i < len(line) - 1:
+            if line.find(line[i:i + 2], i + 2) != -1:
+                doubleconsecutive = True
+        if i < len(line) - 2:
+            if line[i] == line[i + 2]:
+                repeatover = True
+    
+    if not naughty and not doubleconsecutive:
+        naughty = True
+    if not naughty and not repeatover:
+        naughty = True
+    
+    if not naughty:
+        newnice += 1
+
 print("part 1: " + str(nice))
+print("part 2: " + str(newnice))
