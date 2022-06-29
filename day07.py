ff2bd40ea6d8de7e0e6057ed15e30450704dec35
John Gillen <john.gillen@trojans.dsu.edu>
2022-06-29 06:11:55 -0500

    day 07 part 1

---
+++ day07.py
@@ -0,0 +1,44 @@
+f = open("day07.input")
+lines = [line.rstrip() for line in f.readlines()]
+
+circuit = {}
+
+def evaluate(wire):
+    value = 0
+
+    # if wire is int or wire is simple assignment to int
+    if type(wire) == int:
+        value = wire
+    elif type(circuit[wire]) == int:
+        value = circuit[wire]
+    
+    # bitwise operations
+    elif 'AND'      in circuit[wire]:
+        value = evaluate(circuit[wire][0]) &  evaluate(circuit[wire][2])
+    elif 'OR'       in circuit[wire]:
+        value = evaluate(circuit[wire][0]) |  evaluate(circuit[wire][2])
+    elif 'LSHIFT'   in circuit[wire]:
+        value = evaluate(circuit[wire][0]) << evaluate(circuit[wire][2])
+    elif 'RSHIFT'   in circuit[wire]:
+        value = evaluate(circuit[wire][0]) >> evaluate(circuit[wire][2])
+    elif 'NOT'      in circuit[wire]:
+        value = ~ evaluate(circuit[wire][1]) & 0xFFFF # enforce 16bit
+
+    # if wire is simple assignment to another wire
+    else:
+        value = evaluate(circuit[wire][0])
+    
+    circuit[wire] = value
+    return circuit[wire]
+
+def convert(word):
+    try:
+        return int(word)
+    except ValueError:
+        return word
+
+for line in lines:
+    words = list(map(convert, line.split()))
+    circuit[words[-1]] = words[:-2]
+
+print('part 1: ' + str(evaluate('a')))
